7f21fb7496ecf9d02d4bb0e536e080087d415e4b
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-12-13 13:36:43 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+//session start;
+//include funcionts.php
+
+/* validare email: 
+prendere email da post;
+
+se filter_var di email, filter_validate_email {
+    l'indirzzo è valido (altre azioni se valido)
+}else: alert, indirizzo non valido
+} 
+
+in function.php: function validateEmail(email) ritornare filter_var(email, filter validate email funzione di php)
+*/
+
+
+
+?>
